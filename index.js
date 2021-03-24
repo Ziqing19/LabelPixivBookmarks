11f57c044102995973f7a760153c2d5219729960
Ziqing19 <44974769+Ziqing19@users.noreply.github.com>
2021-03-24 01:09:35 -0700

    Adaption to novels

--- index.js
+++ index.js
@@ -2,30 +2,34 @@
 // @name         Pixiv收藏夹自动标签
 // @name:en      Label Pixiv Bookmarks
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  自动为Pixiv收藏夹内未分类的图片打上标签
 // @description:en    A sciprt helps pixiv users to label thier untagged images in the bookmarks.
 // @author       LyuShiWang(original author) Ziqing19
-// @match        https://www.pixiv.net/bookmark.php?*
-// @match        https://www.pixiv.net/bookmark_add.php?*
-// @match        https://www.pixiv.net/*users/*/bookmarks/artworks*
+// @match        https://www.pixiv.net/*bookmark*
+// @match        https://www.pixiv.net/*users/*/bookmarks/*
 // @license      MIT
 // @grant        none
 // ==/UserScript==
-
+ 
 (function() {
     const current_url = window.location.href;
     // Confirm to start label
-    if (current_url === "https://www.pixiv.net/bookmark.php?untagged=1") {
+    if (current_url === "https://www.pixiv.net/bookmark.php?untagged=1" ||
+        current_url === "https://www.pixiv.net/novel/bookmark.php?tag=&untagged=1") {
         window.addEventListener("load", () => {
             if (confirm("是否开始整理？\nStart Labeling?")) {
-                window.location.href = "https://www.pixiv.net/bookmark.php?untagged=1&rest=show&p=1";
+                window.location.href += "&rest=show&p=1";
             }
         })
     }
     // Continue
     else if (current_url === "https://www.pixiv.net/bookmark.php?type=illust_all&p=1") {
         window.location.href = "https://www.pixiv.net/bookmark.php?untagged=1&rest=show&p=1";
+    }
+    // Novel continue
+    else if (current_url === "https://www.pixiv.net/novel/bookmark.php?tag=&rest=show&p=1&untagged=1") {
+        window.location.href = "https://www.pixiv.net/novel/bookmark.php?tag=&untagged=1&rest=show&p=1";
     }
     // Add labels
     else if (current_url.indexOf("bookmark_add") > -1) {
@@ -41,7 +45,7 @@
             let tag_set = [];
             for (let i = image_tags.length - 1; i >= 0; i--) {
                 let string_tag = image_tags[i].innerText;
-
+ 
                 let first = string_tag.indexOf("*");
                 let second = string_tag.indexOf("users");
                 if (second == -1) {
@@ -65,27 +69,21 @@
             button_submit.click();
         })
     }
-    // Select first available image
+    // Select first available edit link
     else if (current_url.indexOf("untagged=1") > -1) {
-        let images = document.getElementsByClassName('display_editable_works')[0].childNodes[0].childNodes;
-        if (images.length > 0) {
-            for (let i = 0; i < images.length; i++) {
-                let image = images[i];
-                // the image has been deleted
-                if (image.childNodes.length !== 7) {
-                    continue;
-                } else {
-                    let href = image.childNodes[6].href;
-                    window.location.href = href;
-                    break;
-                }
+        let edit_icons = document.getElementsByClassName("gtm-old_bookmark-edit_bookmark");
+        if (edit_icons.length > 0) {
+            for (let i = 0; i < edit_icons.length; i++) {
+                let edit_icon = edit_icons[i];
+                window.location.href = edit_icon.href;
+                break;
             }
         } else {
             alert("全部处理完毕！\nAll images have been labeled!");
         }
     }
     // new UI
-    else if (current_url.indexOf("/bookmarks/artworks/%E6%9C%AA%E5%88%86%E9%A1%9E") > -1) {
+    else if (current_url.indexOf("%E6%9C%AA%E5%88%86%E9%A1%9E") > -1) {
         let urls;
         let count = 0;
         let id = setInterval(function getImages() {
@@ -102,7 +100,7 @@
         }, 500);
     }
     // new UI repeat
-    else if (current_url.indexOf("/bookmarks/artworks") > -1) {
+    else if (current_url.indexOf("/bookmarks/") > -1) {
         window.location.href += "/%E6%9C%AA%E5%88%86%E9%A1%9E";
     }
 })();
